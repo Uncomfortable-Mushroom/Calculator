efe6dc1ae1594c1bd8f682d0111c16bfebaec653
hym <1349456160@qq.com>
2017-09-14 10:51:07 +0800

    开始菜单的功能设计

--- app/src/main/java/com/example/lenovo/calculator/MainActivity.java
+++ app/src/main/java/com/example/lenovo/calculator/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.lenovo.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Stack;
@@ -18,6 +21,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button zero,one,two,three,four,five,six
                   ,seven,eight,nine,plus,minus,precent
                   ,spot, CE,C,div,equal,multiplay,backspace;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.help:
+                Toast.makeText(this,"这是帮助",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.exit:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
